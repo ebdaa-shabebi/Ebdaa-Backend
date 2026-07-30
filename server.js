b57358658c94d69ce4274c1c442b8686b894f7b3
Ibrahim <ebrahimshabebi@gmail.com>
2026-07-30 23:13:40 +0300

    login icin

--- server.js
+++ server.js
@@ -54,6 +54,26 @@ const storage = multer.diskStorage({
   },
 });
 
+
+app.post("/login", (req, res) => {
+
+  const { username, password } = req.body;
+
+  if (
+    username === process.env.ADMIN_USERNAME &&
+    password === process.env.ADMIN_PASSWORD
+  ) {
+    return res.json({ success: true });
+  }
+
+  res.status(401).json({
+    success: false,
+    message: "Invalid credentials"
+  });
+
+});
+
+
 const upload = multer({ storage });
 
 // ✅ SERVE FILES
