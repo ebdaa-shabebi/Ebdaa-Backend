1bcf06d28e81adf0cd1ae36e1f1a8e8e3bdf155b
Ibrahim <ebrahimshabebi@gmail.com>
2026-08-02 19:29:04 +0300

    testttt

--- server.js
+++ server.js
@@ -75,15 +75,23 @@ const upload = multer({ storage });
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: false, // IMPORTANT for port 587
-  requireTLS: true,
+  secure: true,
+
+  family: 4,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+
   logger: true,
   debug: true,
 });
+
 // ✅ SERVE FILES
 app.use("/uploads", express.static("uploads"));
 
